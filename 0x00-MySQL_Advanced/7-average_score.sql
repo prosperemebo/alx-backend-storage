a2cb8553e0141441653683ac95798395431ecc30
prosperemebo <emeboprosper@gmail.com>
2024-04-25 21:42:58 +0100

    Task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN u_id INT)
+BEGIN
+	SET @av = (SELECT AVG(score) AS av FROM corrections WHERE user_id = u_id);
+	UPDATE users SET average_score = @av WHERE id = u_id;
+END $$
+DELIMITER;
